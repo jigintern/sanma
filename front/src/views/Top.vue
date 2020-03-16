7b05772ebee04fc2a1d3a0d1fca599377b292fd9
ktpi2000 <katapi2000@gmail.com>
2020-03-17 01:32:35 +0900

    適当用意した文字を表示させる

--- front/src/views/Top.vue
+++ front/src/views/Top.vue
@@ -1,9 +1,78 @@
 <template>
   <div class="home">
     <h1>Top page</h1>
+
+    <v-container>
+      <v-container>
+        <v-row dense>
+          <v-col
+            v-for="(article, index) in articles"
+            :key="index"
+            cols="12"
+            sm="12"
+            lg="6"
+          >
+            <v-card
+              color="blue lighten-3"
+              height="100%"
+              @click="click(article.id)"
+            >
+              <v-card-title class="headline">{{ article.title }}</v-card-title>
+
+              <v-card-subtitle>{{ article.body }}</v-card-subtitle>
+
+              <v-card-actions> </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      articles: [
+        {
+          id: 10,
+          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
+          body:
+            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
+        },
+        {
+          id: 20,
+          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
+          body:
+            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
+        },
+        {
+          id: 30,
+          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
+          body:
+            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
+        },
+        {
+          id: 40,
+          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
+          body:
+            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
+        },
+        {
+          id: 50,
+          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
+          body:
+            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
+        }
+      ]
+    };
+  },
+
+  methods: {
+    click(id) {
+      this.$router.push({ name: "Read-article", params: { id } });
+    }
+  }
+};
 </script>
