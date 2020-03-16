1e5866cb159a136f83853d5731fd415e280df703
ktpi2000 <katapi2000@gmail.com>
2020-03-17 00:15:34 +0900

    ヘッダーの移行

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <v-app>
+    <Header />
     <router-view />
   </v-app>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "App",
+
+  components: {
+    Header
+  },
 
   data: () => ({
     //
--- front/src/views/Top.vue
+++ front/src/views/Top.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="home">
-    <v-app-bar color="light-purple accent-1" dark>
-      <v-toolbar-title>Top</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-col class="text-center" cols="12" sm="1">
-        <div class="my-2">
-          <v-btn large>Normal</v-btn>
-        </div>
-      </v-col>
-    </v-app-bar>
+    <h1>Top page</h1>
   </div>
 </template>
 
