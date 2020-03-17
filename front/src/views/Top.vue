58ea8e77ba619b9aa701f78b925237c4da5d35c8
ktpi2000 <katapi2000@gmail.com>
2020-03-17 15:38:15 +0900

    get先を変えた

--- front/src/views/Top.vue
+++ front/src/views/Top.vue
@@ -4,18 +4,8 @@
       <v-container>
         <div v-show="loading">Loading...</div>
         <v-row dense v-show="!loading">
-          <v-col
-            v-for="(article, index) in articles"
-            :key="index"
-            cols="12"
-            sm="12"
-            lg="6"
-          >
-            <v-card
-              color="blue lighten-3"
-              height="100%"
-              @click="click(article.id)"
-            >
+          <v-col v-for="(article, index) in articles" :key="index" cols="12" sm="12" lg="6">
+            <v-card color="blue lighten-3" height="100%" @click="click(article.id)">
               <v-card-title class="headline">{{ article.title }}</v-card-title>
 
               <v-card-subtitle>{{ article.body | ellipsis }}</v-card-subtitle>
@@ -35,40 +25,6 @@ export default {
     return {
       loading: true,
       articles: []
-      /*
-      articles: [
-        {
-          id: 10,
-          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
-          body:
-            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
-        },
-        {
-          id: 20,
-          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
-          body:
-            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
-        },
-        {
-          id: 30,
-          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
-          body:
-            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
-        },
-        {
-          id: 40,
-          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
-          body:
-            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
-        },
-        {
-          id: 50,
-          title: "Title, title, タイトル, たいとる, ﾀｲﾄﾙ",
-          body:
-            "3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。3分で分かります。高専に任せろ。"
-        }
-      ]
-      */
     };
   },
 
@@ -83,7 +39,7 @@ export default {
   },
 
   mounted: function() {
-    this.$axios.get("https://qiita.com/api/v2/items").then(res => {
+    this.$axios.get("https://sanma.intern.jigd.info/api/articles").then(res => {
       console.log(res);
       this.articles = res.data;
       this.loading = false;
