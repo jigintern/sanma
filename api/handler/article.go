23679f7f8cd68e0054a95b3fca30f449c50a1a74
terfno <sueda.takahito@gmail.com>
2020-03-17 11:10:09 +0900

    :tada: get all articles

--- api/handler/article.go
+++ api/handler/article.go
@@ -8,8 +8,20 @@ import (
 	"sanma/domain"
 )
 
-func NewArticle(c *gin.Context){}
-func GetAllArticles(c *gin.Context){}
-func GetArticlesByAID(c *gin.Context){}
-func UpdateArticle(c *gin.Context){}
-func DeleteArticle(c *gin.Context){}
+func NewArticle(c *gin.Context) {}
+
+func GetAllArticles(c *gin.Context) {
+	articles, err = domain.GetAllArticles()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err,
+		})
+	}
+	c.JSON(http.StatusOK, articles)
+}
+
+func GetArticlesByAID(c *gin.Context) {}
+
+func UpdateArticle(c *gin.Context) {}
+
+func DeleteArticle(c *gin.Context) {}
