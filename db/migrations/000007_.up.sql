e171eb94b1a95657df9330b4acadd1328e0c0887
terfno <sueda.takahito@gmail.com>
2020-03-17 16:03:57 +0900

    :fire: insert dmy

---
+++ db/migrations/000007_.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO users VALUES ("admin", "admin@gmail.com", "admin");
+INSERT INTO articles (author, article_status, article_type, article_url, title) VALUES ("admin", 1, 1, "https://qiita.com//kaba/items/d2fb7b22822f7add19a3", "3分で分かるreact nativeのLayout animation");
+INSERT INTO articles (author, article_status, article_type, article_url, title) VALUES ("admin", 1, 1, "https://qiita.com//tshk_mtsys/items/03177c7d00c50c8c3789", "3分で分かった気になれるJDK12");
+INSERT INTO articles (author, article_status, article_type, article_url, title) VALUES ("admin", 1, 1, "https://qiita.com//kaba/items/bf41bc7eca4335803abf", "3分で分かる！ONNXフォーマットとWindows Machine Learning！");
+INSERT INTO articles (author, article_status, article_type, article_url, title) VALUES ("admin", 1, 1, "https://qiita.com//inasync/items/c653d7a952a873fef8b0", "3分で分かる C# 6 の新機能");
+INSERT INTO articles (author, article_status, article_type, article_url, title) VALUES ("admin", 1, 1, "https://qiita.com//kojingharang/items/5762ed009b67b95f22e1", "3分で分かる Erlang hot code loading");
