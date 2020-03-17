fe7046191a3fd308eb8f160b8d9ccacc4b8650a2
terfno <sueda.takahito@gmail.com>
2020-03-17 12:06:19 +0900

    :fire: write insert status and type

---
+++ db/migrations/000006_insert_as-at.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO article_status VALUES (0, "draft");
+INSERT INTO article_status VALUES (1, "published");
+INSERT INTO article_type VALUES (0, "origin");
+INSERT INTO article_status VALUES (1, "crawled");
