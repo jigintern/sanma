e8152fded5829bbfc41f8aca8379c63dd65fbce4
ktpi2000 <katapi2000@gmail.com>
2020-03-17 09:58:14 +0900

    sign in フォーム実装

--- front/src/views/Signin.vue
+++ front/src/views/Signin.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="signin">
-    <h1>This is a signin page</h1>
-  </div>
+  <v-app>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Sign in</h1>
+      </v-card-title>
+
+      <v-card-text>
+        <v-form>
+          <v-text-field prepend-icon="mdi-email" type="email" label="mail" />
+          <v-text-field prepend-icon="mdi-lock" type="password" label="password" />
+        </v-form>
+
+        <v-card-actions>
+          <v-btn class="info text-none">Login</v-btn>
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
